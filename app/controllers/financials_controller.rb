53ab2070843a84c9ce4bd9dddbb2e06381625151
rnparks <rnparks@gmail.com>
2016-04-15 19:50:16 -0400

    first commit

---
+++ app/controllers/financials_controller.rb
@@ -0,0 +1,14 @@
+class FinancialsController < ApplicationController
+  def show
+    file_url = "ftp://ftp.sec.gov/edgar/data/728535/0001437749-15-018776.txt"
+    @response = Nokogiri::HTML(open(file_url))
+    @shit = @response.css('table')
+
+
+
+
+
+
+
+  end
+end
