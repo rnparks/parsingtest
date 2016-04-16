6441474f552f1876c02e0ba50b0e8f0fb493b6bd
rnparks <rnparks@gmail.com>
2016-04-15 22:11:17 -0400

    changes I don't want

--- app/controllers/financials_controller.rb
+++ app/controllers/financials_controller.rb
@@ -2,7 +2,13 @@ class FinancialsController < ApplicationController
   def show
     file_url = "ftp://ftp.sec.gov/edgar/data/728535/0001437749-15-018776.txt"
     @response = Nokogiri::HTML(open(file_url))
-    @shit = @response.css('table')
+    # @shit = @response.css('table')
+    @shit = {name: "ryan", email: "parks", something: "blah"}
+    test_array = [{name: "ryan", email: "parks", something: "blah"}, {name: "steve", email: "billy", something: "titto"}]
+    @container
+    test_array.each do |index|
+      @container = index
+    end
 
 
   end
